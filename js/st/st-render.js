874858bba9e0092e5cded9c44dfb259182334c1d
Jeff Conrad <ledlogic@gmail.com>
2021-11-12 08:00:59 -0600

    Added work section

--- js/st/st-render.js
+++ js/st/st-render.js
@@ -14,6 +14,7 @@ st.render = {
 		that.renderReset();		
 		that.renderOverview();
 		that.renderAttributes();
+		that.renderWork();
 		that.renderSkills();
 		
 		$(".st-page").removeClass("st-initial-state");
@@ -102,5 +103,29 @@ st.render = {
 			});
 		});
 		st.render.$pageft.append($skills);
+	},
+	renderWork: function() {
+		st.log("rendering work");
+
+		var work = st.character.data.work;		
+		//st.log(work);
+		var $work = $("<div class=\"st-section st-work\"></div>");
+		$work.append("<div class=\"st-section-title\">Work History</div>");
+
+		var h = "<span class=\"st-skill-item-key\">Position</span> "
+		      + "<span class=\"st-skill-item-stat\">Years</span>";
+		var $elm = $("<span class=\"st-skill st-skill-item\">" + h + "</span>");
+		$work.append($elm);
+						
+		_.each(work, function(map) {
+			_.each(map, function(value, key) {
+				//st.log(map);
+				var h = "<span class=\"st-skill-item-key\">" + key + "</span> "
+				      + "<span class=\"st-skill-item-stat\">" + value + "</span>";
+				var $elm = $("<span class=\"st-skill st-skill-item st-skill-item-" + key + "\">" + h + "</span>");
+				$work.append($elm);
+			});
+		});
+		st.render.$pageft.append($work);
 	}
 };
